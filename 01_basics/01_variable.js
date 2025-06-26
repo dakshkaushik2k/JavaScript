ea71b328256ea0184b184ff4e534ed1f6333b7c9
dakshkaushik2k <151449854+dakshkaushik2k@users.noreply.github.com>
2025-06-26 06:18:46 +0000

    updates

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+let accoundNo=123;
+var accoundEmail="abc@gmail.com";
+const accoundActive=true;
+let hellog="Run"
+let accoundBloacked
+
+/* Prefer not to use var
+beacus of the issue in block scope and functional scope
+*/
+
+console.table([accoundNo,accoundEmail,accoundActive,hellog,accoundBloacked]);
